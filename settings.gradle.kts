6fc6a23f52edb2fca02b1f0e97a3e3c6ca386735
Hartmut <info@hartmut.co.uk>
2024-02-09 17:03:35 +0100

    chore: quarkus create project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
-rootProject.name = "java-library-template"
-
-include("java-library-template")
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+rootProject.name="quarkus-gradle-renovate-example"
